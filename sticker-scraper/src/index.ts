54258fab9a43e978627a362f22f0bcdadfd52e9c
Todor Popov <todorpopov02@gmail.com>
2024-08-23 16:17:45 +0300

    fix: "/stickers-api/get/current/delay" returned a number which was casted as a status code by Express

--- sticker-scraper/src/index.ts
+++ sticker-scraper/src/index.ts
@@ -1,5 +1,5 @@
 import { StickerScraper } from "./scraper/StickerScraper.js"
-import express, { Request } from 'express'
+import express from 'express'
 import mongoose from "mongoose"
 import { PersistenceService } from "./persistence/PersistenceService.js"
 
@@ -22,6 +22,7 @@ mongoose.connect('mongodb://mongo:27017/sticker-scraper', {
     }
 )
 
+// use auth middleware
 app.use(function(req, res, next) {
     if (req.headers.auth !== process.env.AUTH) {
         return res.status(403).send("No credentials sent!");
@@ -29,96 +30,95 @@ app.use(function(req, res, next) {
     next();
 });
 
+// start scraping process route
 app.put("/stickers-api/start", (req, res) => {
     try {
         scraper.startScraping()
         res.status(200).send("Scraping process has started!")
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
+// stop scraping process route
 app.put("/stickers-api/stop", (req, res) => {
     try {
         scraper.stopScraping()
         res.status(200).send("Scraping process has ended!")
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
+// get all stickers route
 app.get("/stickers-api/get/stickers", async (req, res) => {
     try {
         const stickers = await PersistenceService.getAll()
         res.status(200).send(stickers)
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
+// get sticker by sticker code
 app.get("/stickers-api/get/sticker/code/:code", async (req, res) => {
     try {
         const code = String(req.params.code)
         const sticker = await PersistenceService.findOneByCode(code)
         res.status(200).send(sticker)
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
+// get sticker by sticker name
 app.get("/stickers-api/get/sticker/name/:name", async (req, res) => {
     try {
         const name = String(req.params.name)
         const sticker = await PersistenceService.findOneByName(name)
         res.status(200).send(sticker)
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
-app.get("/stickers-api/get/current/delay", async (req, res) => {
+// get current scraper delay state
+app.get("/stickers-api/get/current/delay", (req, res) => {
     try {
         const delay = scraper.getCurrentDelay()
-        res.status(200).send(delay)
-    } catch {
-        res.status(500).send("Server error has occured!")
+        res.status(200).send(String(delay))
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
-app.get("/stickers-api/get/current/scraping-status", async (req, res) => {
+// get current scraping status state
+app.get("/stickers-api/get/current/scraping-status", (req, res) => {
     try {
         const currentStatus = scraper.getCurrentScrapeStatus()
         res.status(200).send(currentStatus)
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
-app.get("/stickers-api/get/sticker/name/:name", async (req, res) => {
-    try {
-        const name = String(req.params.name)
-        const sticker = await PersistenceService.findOneByName(name)
-        res.status(200).send(sticker)
-    } catch {
-        res.status(500).send("Server error has occured!")
-    }
-})
-
+// drop all db entries
 app.delete("/stickers-api/drop-all", async (req, res) => {
     try {
         await PersistenceService.dropAll()
         res.status(200).send("Stickers collection has been dropped!")
-    } catch {
-        res.status(500).send("Server error has occured!")
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
 
+// change the scraper delay
 app.put("/stickers-api/delay", (req, res) => {
     try {
         const delay = String(req.query.delay)
         scraper.changeScrapeDelay(delay)
-        res.status(200).send("Scraping delay has been changed!")
-    } catch {
-        res.status(500).send("Server error has occured!")
+        res.status(200).send(`Scraping delay has been changed to ${delay}!`)
+    } catch(err) {
+        res.status(500).send(err)
     }
 })
